73368a3a8d0608eff131d3b5fbfc6b9053a1267a
pareekshit <pareekshit.ankur@weboniselab.com>
2015-10-12 18:56:54 +0530

    Removed unwanted classes

--- src/main/groovy/org/webonise/restlingtodo/ServiceModule.groovy
+++ src/main/groovy/org/webonise/restlingtodo/ServiceModule.groovy
@@ -4,6 +4,7 @@ import groovy.transform.CompileStatic
 import groovy.util.logging.Slf4j
 import restling.guice.modules.RestlingApplicationModule
 
+
 @Slf4j
 @CompileStatic
 class ServiceModule extends RestlingApplicationModule{
@@ -12,7 +13,6 @@ class ServiceModule extends RestlingApplicationModule{
 
     @Override
     void configureCustomBindings() {
-
     }
 
 }
--- src/main/groovy/org/webonise/restlingtodo/ToDoApplication.java
+++
@@ -1,9 +0,0 @@
-package org.webonise.restlingtodo;
-
-import groovy.transform.CompileStatic;
-import groovy.util.logging.Slf4j;
-
-@CompileStatic
-@Slf4j
-public class ToDoApplication {
-}
